0192073fa952ecb32524620eddc14cabe1fbcb85
David North <dmhnorth@gmail.com>
2014-10-16 17:37:23 +0100

    removed redundant comments. (CC. ch 4)

--- BossFightV1/src/main/java/console/Console.java
+++ BossFightV1/src/main/java/console/Console.java
@@ -5,34 +5,15 @@ package console;
  */
 public interface Console {
 
-    /**
-     * to begin playing the game
-     */
     void startConsole();
 
-    /**
-     * close down the console
-     */
-    void closeConsole();
+    void ShutdownConsole();
 
-    /**
-     * reset the console completely
-     */
     void resetConsole();
 
-    /**
-     * set the manager for persistence
-     */
-    void setDataManager();
+    void setPersistentDataManager();
 
-    /**
-     * set the console View
-     */
     void setView();
 
-    /**
-     * set the console game
-     */
     void setGame();
-
 }
