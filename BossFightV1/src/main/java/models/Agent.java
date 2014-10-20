c20982888b4a859e54a7c3bd8c7a18c54c15e2ca
David North <dmhnorth@gmail.com>
2014-10-20 15:08:05 +0100

    Agent interface created with some basic methods.

---
+++ BossFightV1/src/main/java/models/Agent.java
@@ -0,0 +1,19 @@
+package models;
+
+public interface Agent {
+
+    double getHealth();
+
+    double setMaxHealth();
+
+    double getMaxHealth();
+
+    double getDefence();
+
+    double setDefence();
+
+    double setAttack();
+
+    double getAttack();
+
+}
