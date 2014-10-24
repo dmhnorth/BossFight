d3c1a9c57912fa69e8fdd403e83bca7139585db4
David <dmhnorth@gmail.com>
2014-10-24 12:08:28 +0100

    updated rules on the interface. (Liskov)

--- BossFightV1/src/main/java/generators/NameGenerator.java
+++ BossFightV1/src/main/java/generators/NameGenerator.java
@@ -6,7 +6,8 @@ public interface NameGenerator {
      * For getting a randomly generated string name from any source.
      * Should never return the same name twice.
      *
-     * @return a randomly generated name
+     *
+     * @return name no longer than 30 characters long
      */
     String getUniqueName();
 }
