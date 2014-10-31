654a0e2c900f94e325c7708903f6eef172a30e3b
David <dmhnorth@gmail.com>
2014-10-31 14:46:20 +0000

    added test classes to suite

--- BossFightV1/src/test/java/BossFightTestSuite.java
+++ BossFightV1/src/test/java/BossFightTestSuite.java
@@ -1,10 +1,12 @@
 import consoles.textAdventure.ArenaTest;
+import factories.AgentFactoryTest;
+import generators.NameGeneratorSimpleTest;
 import models.AgentTest;
 import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ArenaTest.class, AgentTest.class})
+@Suite.SuiteClasses({ArenaTest.class, AgentTest.class, AgentFactoryTest.class, NameGeneratorSimpleTest.class, })
 public class BossFightTestSuite {
     //nothing required here
 }
